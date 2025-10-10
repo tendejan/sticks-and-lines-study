2704dc28d04779e28d0fed3ae45a400064d219a5
Tom Endejan <Tom.Endejan@gmail.com>
2025-10-10 21:15:18 +0000

    added documentation, improved code clarity

--- src/major_axis_features.py
+++ src/major_axis_features.py
@@ -1,27 +1,23 @@
 from scipy.spatial.transform import Rotation
 import numpy as np
 
-EULER_ORDER = "YZX"
+EULER_ORDER = "YZX" #as defined by the data 'coding' appilcation
 
 VIEW_AXIS = np.array([0,0,-1]) #viewing from the z axis
 
-def compute_main_axis_rotated(axis_vector, euler_x, euler_y, euler_z):
+def rotate_major_axis(axis_vector, euler_x, euler_y, euler_z):
     """rotate the main axis by the given euler sequence"""
     rotation = Rotation.from_euler(EULER_ORDER, (euler_y, euler_z, euler_x), degrees=True)
     rotated_axis = rotation.apply(axis_vector)
     return rotated_axis
 
-def compute_angle_from_vertical(major_axis, view_axis=VIEW_AXIS):
-    projected_vector = major_axis - np.dot(major_axis, view_axis) * view_axis
-    projected_vector_normalized = projected_vector / np.linalg.norm(projected_vector)
-    angle_from_vert = np.arctan2(projected_vector_normalized[1], projected_vector_normalized[0])
-    angle_from_vert_deg = np.rad2deg(angle_from_vert)
-    angle_from_vert_deg = angle_from_vert_deg % 90
-    angle_from_vert_deg = min(angle_from_vert_deg, 90 + angle_from_vert_deg)
-    return angle_from_vert_deg
-
-def compute_angle_from_vertical_dot(major_axis, perceptual_upright=np.array([0,1])):
+def compute_projected_angle_from_vertical(major_axis, perceptual_upright=np.array([0,1])):
+    """Projects the major axis to the xy plane and computes its angle from the perceptual upright"""
     projected_vector = major_axis[:2] # drop the z axis to project to xy plane
-    #take the dot product to get the angle
-    angle_radians = np.acos((np.abs(np.dot(projected_vector, perceptual_upright)))/(np.linalg.norm(projected_vector)*np.linalg.norm(perceptual_upright)))
-    return np.rad2deg(angle_radians)
+    #find the normalization factor
+    norm_factor = np.linalg.norm(projected_vector)*np.linalg.norm(perceptual_upright)
+    # take the dot product and divide by the norm factor
+    shadow = np.dot(projected_vector, perceptual_upright)/norm_factor
+    #find the angle from the absolute value of the normalized shadow
+    angle_radians = np.acos(np.abs(shadow))
+    return np.rad2deg(angle_radians) #convert to degrees and return
